9dfef3aa88d602081e2e5b88f8d70035c582dd32
izzhariz <izzharizdev@gmail.com>
2020-11-16 22:43:34 +0800

    configured cognito user pool and identity pool in CDK

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -1,10 +1,11 @@
 import S3Stack from "./S3Stack";
+import CognitoStack from "./CognitoStack";
 import DynamoDBStack from "./DynamoDBStack";
 
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
 
-  new S3Stack(app, "s3");
+  const s3 = new S3Stack(app, "s3");
 
-  // Add more stacks
+  new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
 }
